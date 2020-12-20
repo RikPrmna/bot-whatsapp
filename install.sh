bd667070b419e816dbc8f90728396854e6952198
Shuichi126 <76103399+Shuichi126@users.noreply.github.com>
2020-12-20 21:29:03 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Ahmad-script/bot-wa
+cd bot-wa
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
+node index.js
