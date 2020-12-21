aa0d4d55180d13a0ec992a4ed144eb60150c1580
Shuichi126 <76103399+Shuichi126@users.noreply.github.com>
2020-12-21 08:03:14 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Shuichi126/bot-whatsapp
+pkg install wget
+pkg install ffmpeg
+pkg install nodejs
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
+node index.js
